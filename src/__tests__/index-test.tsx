7c2717823cd111e099551fa403b8eecdc3461bba
Alexandre Rieux <alexr.3165@gmail.com>
2017-09-17 20:05:17 +0100

    Fix test

--- src/__tests__/index-test.tsx
+++ src/__tests__/index-test.tsx
@@ -1,14 +1,12 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
-import * as TestUtils from 'react-addons-test-utils';
-import { Main } from '../containers/main';
+import * as TestUtils from 'react-dom/test-utils';
+import Main from '../containers/main';
 
 it('Should check if the index page render correctly', () => {
-
   const wrapper = TestUtils.renderIntoDocument(
     <Main/>
   );
-
   const main = ReactDOM.findDOMNode(wrapper);
   expect(main.textContent).toEqual('Yeah it works');
 });
